bd7d288feb157411b577b1cb85efedab5423f494
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-04-26 14:15:21 +0530

    feat: ssh agent add all keys in ~/.ssh/id_*

--- functions/__ssh_agent_start.fish
+++ functions/__ssh_agent_start.fish
@@ -2,4 +2,9 @@ function __ssh_agent_start -d "start a new ssh agent"
   ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
   chmod 600 $SSH_ENV
   source $SSH_ENV > /dev/null
+  for possiblekey in $HOME/.ssh/id_*;
+    if grep -q PRIVATE "$possiblekey"
+        ssh-add "$possiblekey" > /dev/null
+    end
+  end
 end
